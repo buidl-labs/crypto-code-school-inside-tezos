16397ba3eefedde3bcf29c7a3e2bfd9bb7efafcf
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-03-16 21:18:50 +0530

    feat: add monaco editor

--- src/templates/chapter.js
+++ src/templates/chapter.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { graphql, Link } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import Layout from '../components/Layout/layout';
+import { ControlledEditor, monaco } from '@monaco-editor/react';
 
 export const query = graphql`
   query($slug: String!) {
@@ -20,9 +21,9 @@ const Container = styled.div`
   display: grid;
   grid-template-areas:
     'header header header header'
-    'content content contractFile contractFile'
-    'content content editor editor'
-    'content content option option'
+    'content contractFile contractFile contractFile'
+    'content editor editor editor'
+    'content option option option'
     'footer footer footer footer';
 `;
 
@@ -98,12 +99,31 @@ const Content = styled.div`
 
 const ContractFile = styled.div`
   grid-area: contractFile;
+  background: #112425;
+  height: 50px;
+  width: calc(100vw - (100vw / 2.4));
+
+  > p {
+    background: #1b3738;
+    height: 50px;
+    display: inline-block;
+    margin-left: 1rem;
+    color: #729e9f;
+    padding-top: 13px;
+    padding-right: 10px;
+    padding-left: 10px;
+  }
 `;
 const Editor = styled.div`
   grid-area: editor;
 `;
 const Option = styled.div`
   grid-area: option;
+  background: #112425;
+  height: 60px;
+  width: calc(100vw - (100vw / 2.4));
+  display: flex;
+  justify-content: flex-end;
 `;
 
 const ChapterTitle = styled.p`
@@ -164,7 +184,62 @@ const ContentHeader = styled.div`
   padding: 1.5rem;
 `;
 
+const CheckAnswerButton = styled.button`
+  height: 60px;
+  background-color: #18b77e;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  color: #fff;
+  margin: 0 10px;
+`;
+
+const ShowAnswerButton = styled.button`
+  height: 60px;
+  background-color: #162f30;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  color: #fff;
+  margin: 0 10px;
+`;
 const PostTemplate = ({ data: { mdx: post } }) => {
+  const [editorInputValue, setEditorInputValue] = useState('');
+  useEffect(() => {
+    monaco
+      .init()
+      .then(monacoInstance => {
+        monacoInstance.editor.defineTheme('myCustomTheme', {
+          base: 'vs-dark',
+          inherit: true,
+          rules: [
+            { token: 'comment', foreground: '666666', fontStyle: 'italic' },
+            { token: 'keyword', foreground: 'EA4192' },
+            { token: 'number', foreground: '00FF47' },
+            { token: 'string', foreground: 'FA00FF' },
+          ],
+          colors: {
+            'editor.foreground': '#F8F8F8',
+            'editor.background': '#1B3738',
+            'editor.selectionBackground': '#DDF0FF33',
+            'editor.lineHighlightBackground': '#FFFFFF08',
+            'editorCursor.foreground': '#A7A7A7',
+            'editorWhitespace.foreground': '#FFFFFF40',
+          },
+        });
+      })
+      .catch(error =>
+        console.error(
+          'An error occurred during initialization of Monaco: ',
+          error,
+        ),
+      );
+    return () => {
+      // cleanup;
+    };
+  }, []);
   console.log('Data body', post.body);
   return (
     <Layout>
@@ -192,12 +267,31 @@ const PostTemplate = ({ data: { mdx: post } }) => {
             <MDXRenderer>{post.body}</MDXRenderer>
           </div>
         </Content>
-        <ContractFile>filename.py</ContractFile>
-        <Editor>In-Browser editor</Editor>
+        <ContractFile>
+          <p>Contract.py</p>
+        </ContractFile>
+        <Editor>
+          <ControlledEditor
+            height={`calc(100vh - 250px)`}
+            value={editorInputValue}
+            onChange={(_, value) => setEditorInputValue(value)}
+            language="python"
+            theme="myCustomTheme"
+            options={{
+              lineNumbers: true,
+              scrollBeyondLastLine: true,
+              minimap: { enabled: false },
+              scrollbar: { vertical: 'hidden', verticalScrollbarSize: 0 },
+              folding: true,
+              readOnly: false,
+              fontSize: 18,
+              fontFamily: 'Inconsolata',
+            }}
+          />
+        </Editor>
         <Option>
-          options
-          <button>show answer</button>
-          <button>check</button>
+          <ShowAnswerButton> ? Show Answer</ShowAnswerButton>
+          <CheckAnswerButton>TickIcon Check</CheckAnswerButton>
         </Option>
         <Footer>
           <div>
