83e299b43591c1c85789ed8a5a74cd5c4be19e4b
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-03-16 21:18:50 +0530

    feat: add monaco editor

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Layout from '../components/Layout/layout';
 import useChapters from '../hooks/use-chapters';
+import { DiffEditor } from '@monaco-editor/react';
+
 const IndexPage = () => {
   const chapters = useChapters();
   return (
@@ -10,6 +12,23 @@ const IndexPage = () => {
       {chapters.map(chapter => (
         <pre>{JSON.stringify(chapter, null, 2)}</pre>
       ))}
+      <DiffEditor
+        height="440px"
+        original={'yo'}
+        modified={'hi'}
+        language="python"
+        options={{
+          lineNumbers: true,
+          scrollBeyondLastLine: true,
+          minimap: { enabled: false },
+          scrollbar: { vertical: 'hidden', verticalScrollbarSize: 0 },
+          folding: true,
+          readOnly: false,
+          fontSize: 14,
+          fontFamily: 'Electrolize',
+          renderSideBySide: false,
+        }}
+      />
     </Layout>
   );
 };
