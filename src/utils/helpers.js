b9fd4a31733121b85cbf9770339cc298928eb456
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-03-17 23:47:57 +0530

    feat: add functionality to chpater template

---
+++ src/utils/helpers.js
@@ -0,0 +1,29 @@
+/**
+ * return total chapter and current selected chapter
+ * @param {chapters list array} chapterList
+ * @param {current selected chapter} currentSlug
+ * @return {total chapter count,
+ * current slected chapter,
+ * next chapter link,
+ * previous chapter link} object {total, current, nextSlug, prevSlug}
+ */
+export const getChaptersIndex = (chapterList, currentSlug) => {
+  const total = chapterList.length;
+  //current chapter index
+  const current = chapterList.findIndex(chapter => {
+    return chapter.slug === currentSlug;
+  });
+  const nextChapterIndex = current + 1;
+  const prevChapterIndex = current - 1;
+
+  return {
+    total,
+    current: current + 1,
+    nextSlug: chapterList[nextChapterIndex]
+      ? chapterList[nextChapterIndex].slug
+      : '',
+    prevSlug: chapterList[prevChapterIndex]
+      ? chapterList[prevChapterIndex].slug
+      : '',
+  };
+};
