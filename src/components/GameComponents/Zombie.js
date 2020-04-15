50263c64a33948532fdbf6eb63da0a3fab28fbfa
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-04-16 00:05:40 +0530

    ft: created a zombie using different svg parts

---
+++ src/components/GameComponents/Zombie.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import zombieAnatomyList from './ZombieParts';
+
+// Styles
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/core';
+import { useSpring, animated, config } from 'react-spring';
+
+// Animations
+const rotateFace = keyframes`
+    0% { transform: rotate(-5deg); }
+    100% { transform: rotate(5deg); }
+`;
+
+const Zombie = styled.div`
+  position: absolute;
+  bottom: 10%;
+  right: 10%;
+  width: 16vw;
+  height: 20vw;
+  max-width: 150px;
+  max-height: 210px;
+  z-index: 6;
+
+  @media only screen and (max-width: 425px) {
+    width: 25vw;
+    height: 30vw;
+  }
+`;
+
+const Face = styled(zombieAnatomyList.face)`
+  height: 100px;
+  position: absolute;
+  left: 0%;
+  top: 0%;
+  width: 100px;
+  transition: all 1s cubic-bezier(0.43, 0.13, 0.15, 0.99);
+  z-index: 7;
+  animation-name: ${rotateFace};
+  animation-duration: 1s;
+  animation-direction: alternate;
+  animation-iteration-count: infinite;
+  animation-timing-function: cubic-bezier(0.43, 0.13, 0.15, 0.99);
+`;
+
+const Body = styled(zombieAnatomyList.body)`
+  position: absolute;
+  top: 66px;
+  width: 100px;
+  height: 100px;
+  left: 43px;
+  transition: all 1s cubic-bezier(0.43, 0.13, 0.15, 0.99);
+  z-index: 7;
+`;
+
+const BackHand = styled(zombieAnatomyList.backHand)`
+  position: absolute;
+  top: 75px;
+  width: 50px;
+  height: 110px;
+  left: 38px;
+  transform: rotate(25deg);
+  transform-origin: top right;
+  transition: all 1s cubic-bezier(0.43, 0.13, 0.15, 0.99);
+`;
+
+const BackLeg = styled(zombieAnatomyList.backLeg)`
+  position: absolute;
+  top: 125px;
+  width: 50px;
+  height: 110px;
+  transform-origin: top right;
+  left: 50px;
+  transition: all 1s cubic-bezier(0.43, 0.13, 0.15, 0.99);
+  z-index: 4;
+`;
+
+const FrontHand = styled(zombieAnatomyList.frontHand)`
+  position: absolute;
+  top: 75px;
+  width: 50px;
+  height: 110px;
+  left: 55px;
+  transform: rotate(-10deg);
+  transform-origin: top right;
+  transition: all 1s cubic-bezier(0.43, 0.13, 0.15, 0.99);
+  z-index: 8;
+`;
+
+const FrontLeg = styled(zombieAnatomyList.frontLeg)`
+  position: absolute;
+  top: 130px;
+  width: 60px;
+  height: 120px;
+  transform: rotate(-15deg);
+  transform-origin: top left;
+  left: 82px;
+  transition: all 1s cubic-bezier(0.43, 0.13, 0.15, 0.99);
+  z-index: 5;
+`;
+
+const ZombieContainer = () => {
+  return (
+    <Zombie>
+      <Face />
+      <BackHand />
+      <Body />
+      <FrontHand />
+      <BackLeg />
+      <FrontLeg />
+    </Zombie>
+  );
+};
+
+export default ZombieContainer;
