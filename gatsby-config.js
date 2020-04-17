c7dc1dbd201ffdec12769cdcfbc5a22650c50812
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-04-17 21:10:17 +0530

    chore: update pwa manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,13 +40,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Crypto Code School`,
+        short_name: `CSS`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#162F30`,
+        theme_color: `#162F30`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/theme.svg`, // This path is relative to the root of the site.
       },
     },
     {
