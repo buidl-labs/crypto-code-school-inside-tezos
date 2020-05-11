a4d434677757849e8d757234c45227d4c801f576
Prastut <kr.prastut@gmail.com>
2020-05-11 15:13:21 +0530

    Merged dev fully + changed to Github theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,5 @@
 import Typography from 'typography';
-import altonTheme from 'typography-theme-alton';
+import githubTheme from 'typography-theme-github';
 
-const typography = new Typography(altonTheme);
+const typography = new Typography(githubTheme);
 export default typography;
