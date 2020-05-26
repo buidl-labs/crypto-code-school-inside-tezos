ca02eddee296235e0f0e7daed00075ae989ecdfd
Prastut <kr.prastut@gmail.com>
2020-05-26 14:09:21 +0530

    copy review finished till chapter 11

--- src/components/PlantGrowthModal/index.js
+++ src/components/PlantGrowthModal/index.js
@@ -10,14 +10,14 @@ import { OutboundLink } from 'gatsby-plugin-amplitude-analytics';
 import { PLANT_GROWTH } from '../Plants/PLANT_GROWTH';
 import StyledLink, { CustomLink } from '../StyledLink';
 //TODO: Stage update doesn't work properly
-const cryptobotsParts = ['legs', 'abdomen', 'chest + shooter'];
+const cryptobotsParts = ['legs', 'abdomen', 'chest + shooter', 'head'];
 
 const stages = [
   {
     stage: 0,
     description: {
       before:
-        'Virtual highfive! You have successfully completed stage 1 of your learning 👏👏\n You can now power on the cryptobot parts building machine to assemble your first part!',
+        'Virtual highfive! You have successfully completed stage 1 of your learning 👏👏\n You can now power on the cryptobot parts building machine to assemble your 1st part!',
       after: `Congratulations! You were able to build the ${cryptobotsParts[0]} of your cryptobot!`,
     },
     nextChapterLink: '',
@@ -26,7 +26,7 @@ const stages = [
     stage: 1,
     description: {
       before:
-        'Virtual highfive! You have successfully completed stage 2 of your learning 👏👏\n You can now power on the cryptobot parts building machine to assemble your second part!',
+        'Virtual highfive! You have successfully completed stage 2 of your learning 👏👏\n You can now power on the cryptobot parts building machine to assemble your 2nd part!',
       after: `Congratulations! You were able to build the ${cryptobotsParts[0]} of your cryptobot!`,
     },
     nextChapterLink: '',
@@ -35,7 +35,7 @@ const stages = [
     stage: 2,
     description: {
       before:
-        'Virtual highfive! You have successfully completed stage 3 of your learning 👏👏\n You can now power on the cryptobot parts building machine to assemble your third part!',
+        'Virtual highfive! You have successfully completed stage 3 of your learning 👏👏\n You can now power on the cryptobot parts building machine to assemble your 3rd part!',
       after: `Congratulations! You were able to build the ${cryptobotsParts[1]} of your cryptobot!`,
     },
     nextChapterLink: '',
@@ -44,7 +44,7 @@ const stages = [
     stage: 3,
     description: {
       before:
-        'You have successfully completed this chapter. You can now try to evolve your CryptoBot to stage four.',
+        'Virtual highfive! You have successfully completed stage 4 of your learning 👏👏\n You can now power on the cryptobot parts building machine to assemble your 4th part!',
       after: `Congratulations! You were able to build the ${cryptobotsParts[2]} of your cryptobot!`,
     },
     nextChapterLink: '',
@@ -54,8 +54,7 @@ const stages = [
     description: {
       before:
         'You have successfully completed this chapter. You can now try to evolve your CryptoBot to stage five.',
-      after:
-        "You have successfully completed the chapter and evolved your CryptoBot's gem into stage five.",
+      after: `Congratulations! You were able to build the ${cryptobotsParts[3]} of your cryptobot!`,
     },
     nextChapterLink: '',
   },
