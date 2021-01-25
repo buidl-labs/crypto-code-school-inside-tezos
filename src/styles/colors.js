8763a4f9e34234be5c11b09f2e085cbc2e783026
Manan Gouhari <gouhari21@hotmail.com>
2021-01-25 13:38:00 +0530

    feat: boilerplate for colors & spacing added.

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export const baseColors = {};
+
+export const primaryColors = {};
+
+export const greenAccent = {};
+
+export const redAccent = {};
+
+export const editorColors = {};
+
+export const error = '';
+
+export const success = '';
