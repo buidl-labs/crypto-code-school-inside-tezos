62bb5444d325307e129c08ba005bf4b123f2947d
Manan Gouhari <gouhari21@hotmail.com>
2021-01-25 16:31:28 +0530

    feat: typography vars added.

---
+++ src/styles/typography.js
@@ -0,0 +1,47 @@
+export const typography = {
+  h1: {
+    fontSize: '64px',
+    fontWeight: 900,
+  },
+  h2: {
+    fontSize: '48px',
+    fontWeight: 900,
+  },
+  h3: {
+    fontSize: '36px',
+    fontWeight: 900,
+  },
+  h4: {
+    fontSize: '24px',
+    fontWeight: 800,
+  },
+  h5: {
+    fontSize: '18px',
+    fontWeight: 800,
+  },
+  body1: {
+    fontSize: '24px',
+  },
+  body2: {
+    fontSize: '18px',
+  },
+  button: {
+    fontSize: '24px',
+    fontWeight: 700,
+  },
+  link: {
+    fontSize: '18px',
+    fontWeight: 700,
+  },
+  captionBold: {
+    fontSize: '24px',
+    fontWeight: 700,
+  },
+  overline: {
+    fontSize: '14px',
+  },
+  overlineBold: {
+    fontSize: '14px',
+    fontWeight: 700,
+  },
+};
