3fd93899f8783fa321cdbd534880c87b27b673f0
Manan Gouhari <gouhari21@hotmail.com>
2021-03-12 16:06:53 +0530

    checkIfUserActive function refactored

---
+++ src/utils/automaticLogin.js
@@ -0,0 +1,29 @@
+import { batchUpdateProgress } from 'src/api';
+
+async function checkIfUserActive(setUser, beacon, location, from = '') {
+  if (typeof window == 'undefined' || typeof beacon == 'undefined') return;
+  console.log(location);
+  if (from == 'customizer') {
+    localStorage.setItem('last-page', '/tezos/customizebot');
+  }
+
+  const acc = await beacon.client.getActiveAccount();
+
+  if (acc) {
+    const u = JSON.parse(localStorage.getItem('user') || '{}');
+
+    if (u && acc.address === u.xtzAddress) {
+      if (u.verified) {
+        setUser(u);
+        let progress =
+          typeof window !== `undefined` && localStorage.getItem('progress');
+        if (progress) {
+          progress = JSON.parse(progress);
+          const res = await batchUpdateProgress(u, progress);
+        }
+      }
+    }
+  }
+}
+
+export default checkIfUserActive;
