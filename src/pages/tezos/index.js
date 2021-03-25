d63762bc47dc30ac1191cb4d45f9fd74e80aa7b2
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2021-03-25 23:23:10 +0530

    fix: buidl error

--- src/pages/tezos/index.js
+++ src/pages/tezos/index.js
@@ -65,25 +65,25 @@ import { isMobile, isTablet } from 'react-device-detect';
 //   );
 // };
 
-const TestimonialCard = ({ link, img, name, username, text }) => {
-  return (
-    <a
-      className="h-full text-center bg-base-700 border-2 border-base-400 p-6 rounded-md transform transition ease-in-out duration-500 hover:scale-95"
-      href={link}
-      rel="noopener"
-      target="_blank"
-    >
-      <img
-        alt="testimonial"
-        className="w-20 h-20 mb-3 object-cover object-center rounded-full inline-block"
-        src={img}
-      />
-      <h4 className="text-white font-bold text-xl mb-1">{name}</h4>
-      <p className="text-white text-base mb-2">@{username}</p>
-      <p className="text-base-50 text-center">{text}</p>
-    </a>
-  );
-};
+// const TestimonialCard = ({ link, img, name, username, text }) => {
+//   return (
+//     <a
+//       className="h-full text-center bg-base-700 border-2 border-base-400 p-6 rounded-md transform transition ease-in-out duration-500 hover:scale-95"
+//       href={link}
+//       rel="noopener"
+//       target="_blank"
+//     >
+//       <img
+//         alt="testimonial"
+//         className="w-20 h-20 mb-3 object-cover object-center rounded-full inline-block"
+//         src={img}
+//       />
+//       <h4 className="text-white font-bold text-xl mb-1">{name}</h4>
+//       <p className="text-white text-base mb-2">@{username}</p>
+//       <p className="text-base-50 text-center">{text}</p>
+//     </a>
+//   );
+// };
 
 function Landing() {
   const [videoModal, setVideoModal] = useState(0);
