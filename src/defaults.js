358dc0fbc0b5be6ec7fc4910060f08a8505cbc90
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2021-03-29 22:15:21 +0530

    feat: add mainnet contarct

--- src/defaults.js
+++ src/defaults.js
@@ -3,7 +3,7 @@ import { NetworkType } from '@airgap/beacon-sdk';
 export const APP_NAME = 'Cryptocodeschool';
 
 // allowed: 'mainnet', 'carthagenet', 'labnet' or 'sandbox'(http://localhost:8732) https://delphinet.smartpy.io
-export const NETWORK = NetworkType.EDONET;
-export const INDEXER_NETWORK = 'edo2net';
+export const NETWORK = NetworkType.MAINNET;
+export const INDEXER_NETWORK = 'mainnet';
 
-export const CONTRACT_ADDRESS = 'KT1PaRe6pHNZi8A4cNTzVZjjUMpomu7K97zK';
+export const CONTRACT_ADDRESS = 'KT1V6cNW5jTUxEwmMhxvNHkMF3Bkm5a9Cfrt';
