1aa858cde55a0fb89d0fb51ea6c75e41976c483d
Harsh Badhai <49694914+harsh242@users.noreply.github.com>
2021-04-01 22:44:22 +0530

    Feat:Customize loader of model-viewer (#332)

--- src/pages/tezos/transaction.js
+++ src/pages/tezos/transaction.js
@@ -256,7 +256,16 @@ function Transaction({ location }) {
               src={`https://cloudflare-ipfs.com/ipfs/${bot ? bot.uri : ''}`}
               auto-rotate
               rotation-per-second="15deg"
-            ></model-viewer>
+              id="cryptobot"
+            >
+              <div
+                slot="poster"
+                className="text-white flex justify-center pt-12 text-center"
+              >
+                Please Wait... <br/>
+                Loading 3D Cryptobot
+              </div>
+            </model-viewer>
           </div>
           <div className="px-12 pt-4 ">
             <div className="flex items-center">
